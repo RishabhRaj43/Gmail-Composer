d35ee1bb2b9cd51fac6d45f4c722758c3fbde448
RishabhRaj14 <rishabhraj4675@gmail.com>
2024-08-05 08:37:29 +0530

    first commited

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-accent">Accent</button>
+    </div>
+  );
+}
+
+export default App;
